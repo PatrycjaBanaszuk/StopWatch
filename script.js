d3687491742908fa7d08cde4d4da508eb4a81dd0
Patrycja <pwbanaszuk@gmail.com>
2022-02-12 21:24:26 +0000

    added new function to the buttons

---
+++ script.js
@@ -0,0 +1,71 @@
+const startBtn = document.querySelector('.start')
+const pauseBtn = document.querySelector('.pause')
+const stopBtn = document.querySelector('.stop')
+const resetBtn = document.querySelector('.reset')
+const historyBtn = document.querySelector('.history')
+const stopwatch = document.querySelector('.stopwatch')
+const time = document.querySelector('.time')
+const timelist = document.querySelector('.time-list')
+const infoBtn = document.querySelector('.info')
+const modalBtn = document.querySelector('.modal-shadow')
+const closeModalBtn = document.querySelector('.close')
+
+let countTime
+let minutes = 0
+let seconds = 0
+let timesArr = []
+
+const handleStart = () => {
+	clearInterval(countTime)
+
+	countTime = setInterval(() => {
+		if (seconds < 9) {
+			seconds++
+			stopwatch.textContent = `${minutes}:0${seconds}`
+		} else if (seconds >= 9 && seconds < 59) {
+			seconds++
+			stopwatch.textContent = `${minutes}:${seconds}`
+		} else {
+			minutes++
+			seconds = 0
+			stopwatch.textContent = `${minutes}:00`
+		}
+	}, 1000)
+}
+
+const handleStop = () => {
+	time.innerHTML = `${stopwatch.textContent}`
+
+	if (stopwatch.textContent !== '0:00') {
+		time.style.visibility = 'visible'
+		timesArr.push(stopwatch.textContent)
+	}
+
+	clearStuff()
+}
+
+const handlePause = () => {
+	clearInterval(countTime)
+}
+
+const handleReset = () => {
+
+	time.style.visibility = 'hidden'
+	timesArr = []
+
+	clearStuff()
+}
+
+const clearStuff = () => {
+	clearInterval(countTime)
+	stopwatch.textContent = '0:00'
+	timelist.textContent = ''
+	seconds = 0
+	minutes = 0
+}
+
+startBtn.addEventListener('click', handleStart)
+pauseBtn.addEventListener('click', handlePause)
+stopBtn.addEventListener('click', handleStop)
+resetBtn.addEventListener('click', handleReset)
+
